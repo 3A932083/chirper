f2578a2c10c8cfc5a7fce16ac876cdb10c84929e
jiang-shu-yi <3a932083@gm.student.ncut.edu.tw>
2023-01-04 10:52:09 +0800

    在user的Models新增對chirp的關聯

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //一個使用者用有多個留言
+    public function chirps()
+    {
+        return $this->hasMany(Chirp::class);
+    }
 }
